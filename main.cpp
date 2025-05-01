22867768712f039f962012eacdcbde88aedf8218
Kauã Arnaut <126538678+KauaArnaut@users.noreply.github.com>
2025-04-30 20:11:21 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-   int num;
-   int *p = &num;
-   cout <<"Informe um numero inteiro: ";
-   cin >>num;
-   cout <<"O valor digitado foi: "<< num << endl;
-   cout <<"Endereco da memoria onde esse numero esta armazenado: "<< &num << endl;
-   cout << "Conteudo apontado pelo ponteiro *p: " << *p << endl;
+    int *vetor = new int[5];
 
+    for(int i = 0;i < 5; i++) {
+        cout <<"Digite o valor para a posicao "<< i <<": ";
+        cin >> vetor[i];
+    }
+
+    cout <<"Valores digitados no vetor: "<< endl;
+    for(int i = 0; i < 5; i++){
+        cout <<"Posicao " << i << "; " << (vetor[i]) << endl;
+    }
 }
